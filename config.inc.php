80a68a7cef4a5d3683da171537f4f10ab2d3fac5
Hirbod <info@nightstomp.com>
2014-02-05 10:26:34 +0100

    fixed error in config file
    fixed an error in config file to prevent problems with installer addon

--- config.inc.php
+++ config.inc.php
@@ -7,7 +7,7 @@
 	 * @author Thomas Göllner, Hirbod Mirjavadi info[at]nightstomp.com
 	 *
 	 * @package redaxo4.3.x, redaxo4.4.x, redaxo4.5.x
-	 * @version 3.0.4
+	 * @version 1.5.1
 	 */
 
 	// ADDON IDENTIFIER
@@ -15,19 +15,19 @@
 
 	// ADDON VERSION
 	////////////////////////////////////////////////////////////////////////////////
-	$REX['ADDON'][$myself]['VERSION'] = array
+	$REX['ADDON'][$mypage]['VERSION'] = array
 	(
 	'VERSION' => 1,
 	'MINORVERSION' => 5,
-	'SUBVERSION' => 0
+	'SUBVERSION' => 1
 	);
 
 	// unique id
 	$REX['ADDON']['rxid'][$mypage] = '1150';
 	// foldername
 	$REX['ADDON']['page'][$mypage] = $mypage;    
-	$REX['ADDON']['version'][$myself] = implode('.', $REX['ADDON'][$myself]['VERSION']);
-	$REX['ADDON']['author'][$myself] = 'Thomas Göllner, Hirbod Mirjavadi';
+	$REX['ADDON']['version'][$mypage] = implode('.', $REX['ADDON'][$mypage]['VERSION']);
+	$REX['ADDON']['author'][$mypage] = 'Thomas Göllner, Hirbod Mirjavadi';
 	$REX['ADDON']['name'][$mypage] = 'Language Copy';
 	$REX['ADDON']['perm'][$mypage] = 'rex_language_copy[]';
 	$REX['PERM'][] = 'rex_language_copy[]';
